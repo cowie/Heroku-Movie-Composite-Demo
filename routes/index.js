7a028fa05f25c91777dab4d7b3138a52badea2b4
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-04 18:43:00 -0800

    i really am not great at basic stuff these days lol

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const router = express.Router();
-const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool(
-  connectionString,
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const moviePageQuery = 'SELECT "title__c", ( SELECT STRING_AGG("keyword__c"."name", \', \') AS "Keywords" FROM "salesforce"."keyword__c" LEFT OUTER JOIN "salesforce"."moviekeyword__c" ON "keyword__c"."external_id__c" = "moviekeyword__c"."keyword__r__external_id__c" WHERE "moviekeyword__c"."movie__r__external_id__c" = $1 ), ( SELECT STRING_AGG("production_company__c"."name", \', \') AS "Production Companies" FROM "salesforce"."production_company__c" LEFT OUTER JOIN "salesforce"."movieproductioncompany__c" ON "production_company__c"."external_id__c" = "movieproductioncompany__c"."production_company__r__external_id__c" WHERE "movieproductioncompany__c"."movie__r__external_id__c" = $1), (SELECT STRING_AGG("production_country__c"."name", \', \') AS "Production Countries" FROM "salesforce"."production_country__c" LEFT OUTER JOIN "salesforce"."movieproductioncountry__c" ON "production_country__c"."iso_3166_1__c" = "movieproductioncountry__c"."production_country__r__iso_3166_1__c" WHERE "movieproductioncountry__c"."movie__r__external_id__c" = $1),(SELECT STRING_AGG("genre__c"."name", \', \') AS "Genres" FROM "salesforce"."genre__c" LEFT OUTER JOIN "salesforce"."moviegenre__c" ON "genre__c"."external_id__c" = "moviegenre__c"."genre__r__external_id__c" WHERE "moviegenre__c"."movie__r__external_id__c" = $1 )FROM "salesforce"."movie__c" WHERE "external_id__c" = $1';
 
@@ -45,7 +44,7 @@ router.get('/movies/:movieID', (req, res, next) => {
   pool.query(moviePageQuery, [movieID], (err, qres) => {
     if (err) {
       console.log(err.stack);
-      res.send(200);
+      res.sendStatus(200);
     } else {
       console.log(qres.rows[0]);
       res.render('movieView', qres.rows[0]);
