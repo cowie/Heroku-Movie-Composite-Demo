5fbec0f32973faa6216fc47e1b27910a00844043
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-05 08:33:11 -0800

    right thats not how you do pug

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,7 @@ router.get('/movies/:movieID', (req, res, next) => {
       res.sendStatus(200);
     } else {
       console.log(qres.rows[0]);
-      res.render('movieView', qres.rows[0]);
+      res.render('movieView', { movie: qres.rows[0] });
     }
   });
 });
