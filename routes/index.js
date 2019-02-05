365faeb33d72bb777a2b54487479941ef67061d1
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-05 13:46:16 -0800

    fixing query

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,8 @@ router.get('/movies/:movieID', (req, res, next) => {
       res.sendStatus(200);
     } else {
       const movie = qres.rows[0];
-      movie.shortCast = JSON.parse(movie.temp_cast__c).slice(0, 3);
+      //movie.shortCast = JSON.parse(movie.temp_cast__c).slice(0, 3);
+      console.log(movie.temp_cast__c);
       movie.previouslyViewedList = [];
       movie.recommendedList = [];
       console.log(movie);
