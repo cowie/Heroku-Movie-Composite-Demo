2b41d7d8dcbd8320b2b8dffd02dcaab1d8f225b8
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-05 11:38:45 -0800

    oh right

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const moviePageQuery = 'SELECT "title__c", ( SELECT STRING_AGG("keyword__c"."name", \', \') AS "Keywords" FROM "salesforce"."keyword__c" LEFT OUTER JOIN "salesforce"."moviekeyword__c" ON "keyword__c"."external_id__c" = "moviekeyword__c"."keyword__r__external_id__c" WHERE "moviekeyword__c"."movie__r__external_id__c" = $1 ), ( SELECT STRING_AGG("production_company__c"."name", \', \') AS "Production Companies" FROM "salesforce"."production_company__c" LEFT OUTER JOIN "salesforce"."movieproductioncompany__c" ON "production_company__c"."external_id__c" = "movieproductioncompany__c"."production_company__r__external_id__c" WHERE "movieproductioncompany__c"."movie__r__external_id__c" = $1), (SELECT STRING_AGG("production_country__c"."name", \', \') AS "Production Countries" FROM "salesforce"."production_country__c" LEFT OUTER JOIN "salesforce"."movieproductioncountry__c" ON "production_country__c"."iso_3166_1__c" = "movieproductioncountry__c"."production_country__r__iso_3166_1__c" WHERE "movieproductioncountry__c"."movie__r__external_id__c" = $1),(SELECT STRING_AGG("genre__c"."name", \', \') AS "Genres" FROM "salesforce"."genre__c" LEFT OUTER JOIN "salesforce"."moviegenre__c" ON "genre__c"."external_id__c" = "moviegenre__c"."genre__r__external_id__c" WHERE "moviegenre__c"."movie__r__external_id__c" = $1 )FROM "salesforce"."movie__c" WHERE "external_id__c" = $1';
+const moviePageQuery = 'SELECT "title__c", "temp_cast__c", overview__c, ( SELECT STRING_AGG("keyword__c"."name", \', \') AS "Keywords" FROM "salesforce"."keyword__c" LEFT OUTER JOIN "salesforce"."moviekeyword__c" ON "keyword__c"."external_id__c" = "moviekeyword__c"."keyword__r__external_id__c" WHERE "moviekeyword__c"."movie__r__external_id__c" = $1 ), ( SELECT STRING_AGG("production_company__c"."name", \', \') AS "companies" FROM "salesforce"."production_company__c" LEFT OUTER JOIN "salesforce"."movieproductioncompany__c" ON "production_company__c"."external_id__c" = "movieproductioncompany__c"."production_company__r__external_id__c" WHERE "movieproductioncompany__c"."movie__r__external_id__c" = $1), (SELECT STRING_AGG("production_country__c"."name", \', \') AS "countries" FROM "salesforce"."production_country__c" LEFT OUTER JOIN "salesforce"."movieproductioncountry__c" ON "production_country__c"."iso_3166_1__c" = "movieproductioncountry__c"."production_country__r__iso_3166_1__c" WHERE "movieproductioncountry__c"."movie__r__external_id__c" = $1),(SELECT STRING_AGG("genre__c"."name", \', \') AS "Genres" FROM "salesforce"."genre__c" LEFT OUTER JOIN "salesforce"."moviegenre__c" ON "genre__c"."external_id__c" = "moviegenre__c"."genre__r__external_id__c" WHERE "moviegenre__c"."movie__r__external_id__c" = $1 )FROM "salesforce"."movie__c" WHERE "external_id__c" = $1';
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle pg client', err);
@@ -48,7 +48,8 @@ router.get('/movies/:movieID', (req, res, next) => {
     } else {
       console.log(qres.rows[0]);
       const { movie } = qres.rows[0];
-      movie.overview__c = movie.thing.split('|')[2];
+      console.log(movie);
+
       res.render('movieView', { movie });
     }
   });
