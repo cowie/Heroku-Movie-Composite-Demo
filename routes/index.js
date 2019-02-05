3ed1a8388ee7e2880df3edbe6c8c4551b530c6e7
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2019-02-04 16:58:13 -0800

    first swag at pg and my terrible psql

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const pool = new Pool(
   connectionString,
 );
 
+const moviePageQuery = 'SELECT "title__c", ( SELECT STRING_AGG("keyword__c"."name", \', \') AS "Keywords" FROM "salesforce"."keyword__c" LEFT OUTER JOIN "salesforce"."moviekeyword__c" ON "keyword__c"."external_id__c" = "moviekeyword__c"."keyword__r__external_id__c" WHERE "moviekeyword__c"."movie__r__external_id__c" = $1 ), ( SELECT STRING_AGG("production_company__c"."name", \', \') AS "Production Companies" FROM "salesforce"."production_company__c" LEFT OUTER JOIN "salesforce"."movieproductioncompany__c" ON "production_company__c"."external_id__c" = "movieproductioncompany__c"."production_company__r__external_id__c" WHERE "movieproductioncompany__c"."movie__r__external_id__c" = $1), (SELECT STRING_AGG("production_country__c"."name", \', \') AS "Production Countries" FROM "salesforce"."production_country__c" LEFT OUTER JOIN "salesforce"."movieproductioncountry__c" ON "production_country__c"."iso_3166_1__c" = "movieproductioncountry__c"."production_country__r__iso_3166_1__c" WHERE "movieproductioncountry__c"."movie__r__external_id__c" = $1),(SELECT STRING_AGG("genre__c"."name", \', \') AS "Genres" FROM "salesforce"."genre__c" LEFT OUTER JOIN "salesforce"."moviegenre__c" ON "genre__c"."external_id__c" = "moviegenre__c"."genre__r__external_id__c" WHERE "moviegenre__c"."movie__r__external_id__c" = $1 )FROM "salesforce"."movie__c" WHERE "external_id__c" = $1';
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle pg client', err);
@@ -25,8 +26,6 @@ router.get('/', (req, res) => {
     productionCompanyList: 'Uh Sony? And like somethign',
     productionCountryList: 'USA!, Pandora Jewelers',
   };
-  
-  
   const previouslyViewedList = 'Dances w Wolves, Ferngully';
   const recommendationList = 'lol like so many movies';
   const reviewsList = '"It Stinks!" - The Critic';
@@ -41,9 +40,9 @@ router.get('/', (req, res) => {
 });
 
 router.get('/movies/:movieID', (req, res, next) => {
-  pool.query('SELECT NOW()', (err,qres) => {
+  const { movieID } = req.params;
+  pool.query(moviePageQuery, [movieID], (err, qres) => {
     console.log(qres);
-    
   });
 });
 
